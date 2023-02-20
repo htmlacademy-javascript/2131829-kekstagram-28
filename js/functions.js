d5c2b836ca81da5a7a36042421886dcd3c07e3a1
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2023-02-20 19:00:43 +0500

    fixup! Made new functions for module2-task1

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,39 @@
 const checkLength = (string, maxLength) => string.length <= maxLength;
 
-function isPalindrome (string) {
+const isPalindrome = (string) => {
   string = string.replaceAll(' ','').toLowerCase();
+
   for (let i = 0; i <= Math.ceil(string.length / 2); i++) {
     if (string.at(i) !== string.at(-i - 1)) {
       return false;
     }
   }
-  return true;
-}
 
-function getInteger(string) {
+  return true;
+};
+
+const getInteger = (string) => {
   string = string.toString().replaceAll(' ', '');
   let number = '';
-  for (let i = 0; i < string.length; i++) {
-    if (!isNaN(Number(string[i]))) {
-      number = number + string[i];
+
+  for (const symbol of string) {
+    if (Number.isInteger(Number(symbol))) {
+      number = number + symbol;
     }
   }
-  return parseInt(number, 10);
-}
 
-function fillString (string, minLength, additionalSymbols) {
+  return parseInt(number, 10);
+};
+
+const fillString = (string, minLength, additionalSymbols) => {
   while (string.length < minLength) {
     string = additionalSymbols.slice(0, minLength - string.length) + string;
   }
+
   return string;
-}
+};
+
+checkLength('test', 1);
+isPalindrome('test');
+getInteger('test');
+fillString('test', 1, 'test');
