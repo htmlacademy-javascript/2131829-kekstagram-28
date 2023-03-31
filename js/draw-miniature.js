fe3281cb7c7787efd45ed78674a5963951e371f5
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2023-03-31 12:03:02 +0500

    removed redundant variables and description variable to createMiniature

--- js/draw-miniature.js
+++ js/draw-miniature.js
@@ -3,14 +3,13 @@ import {createDescriptions} from './photo-description-data.js';
 const pictureTemplate = document.querySelector('#picture').content;
 
 export const createMiniature = (description) => {
-  const miniature = pictureTemplate.cloneNode(true);
-  const miniatureImage = miniature.querySelector('.picture__img');
-  const miniatureComments = miniature.querySelector('.picture__comments');
-  const miniatureLikes = miniature.querySelector('.picture__likes');
+  const miniatureDescription = description; //for transfering description to big picture
 
-  miniatureImage.src = description.url;
-  miniatureComments.textContent = description.comments.length;
-  miniatureLikes.textContent = description.likes;
+  const miniature = pictureTemplate.cloneNode(true);
+
+  miniature.querySelector('.picture__img').src = miniatureDescription.url;
+  miniature.querySelector('.picture__comments').textContent = miniatureDescription.comments.length;
+  miniature.querySelector('.picture__likes').textContent = miniatureDescription.likes;
 
   return miniature;
 };
