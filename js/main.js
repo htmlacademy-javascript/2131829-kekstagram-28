96a0408db13e7aff5cb0ef42b9d930e58104aa3a
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2023-04-01 20:48:05 +0500

    module8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 import {createDescriptions} from './photo-description-data.js';
 import {renderPhotos} from './gallery.js';
+import {showPhoto} from './photo-pop-up-controller.js';
+import {closeShownPhoto} from './photo-pop-up-controller.js';
 
 const descriptions = createDescriptions(25);
 
 renderPhotos(descriptions);
+
+const testDesc = createDescriptions(1);
+
+showPhoto(testDesc[0].url, testDesc[0].likes , testDesc[0].comments, testDesc[0].description);
+
+document.querySelector('.big-picture__cancel').addEventListener('click', closeShownPhoto);
