15aa9909c489764494167c07f3afa999284e53a3
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2023-04-05 01:26:56 +0500

    some changes

---
+++ js/form.js
@@ -0,0 +1,47 @@
+//form class="img-upload__form"
+
+export const imageLoader = document.querySelector('#upload-file');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const documentBody = document.querySelector('body');
+const imageLoaderForm = document.querySelector('.img-upload__form');
+const imageLoaderFormSubmitButton = imageLoaderForm.querySelector('.img-upload__submit');
+
+export const loadForm = () => {
+  imgUploadOverlay.classList.remove('hidden');
+  documentBody.classList.add('modal-open');
+};
+
+export const closeLoaderForm = () => {
+  imgUploadOverlay.classList.add('hidden');
+  documentBody.classList.remove('modal-open');
+  imageLoader.value = '';
+  imageLoader.file = '';
+};
+
+/*
+Напишите код для валидации формы добавления изображения,
+используя библиотеку Pristine (/vendor/pristine). Список полей для валидации:
+
+    Хэш-теги
+    Комментарий
+
+Реализуйте логику проверки так, чтобы, как минимум,
+она срабатывала при попытке отправить форму и не давала этого сделать,
+если форма заполнена не по правилам.
+При желании, реализуйте проверки сразу при вводе значения в поле.
+*/
+
+const pristine = new Pristine(imageLoaderForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--invalid',
+  successClass: 'img-upload__field-wrapper--valid',
+  errorTextParent: 'form__item',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+}, false);
+
+imageLoaderFormSubmitButton.addEventListener('click', () => {
+  pristine.validate();
+  console.log('submit');
+});
+
--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 import {createDescriptions} from './photo-description-data.js';
 import {renderPhotos} from './gallery.js';
+import {loadForm} from './form.js';
+import {closeLoaderForm} from './form.js';
+import {imageLoader} from './form.js';
 
 const descriptions = createDescriptions(25);
 
 renderPhotos(descriptions);
+
+imageLoader.addEventListener('change', loadForm);
+
+const closeImageLoaderButton = document.querySelector('#upload-cancel');
+
+closeImageLoaderButton.addEventListener('click', closeLoaderForm);
